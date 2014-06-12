d518e44b9d44f785bf223c413001802b2c152101
Roman Truba <dreddtru@mail.ru>
2014-06-12 22:09:56 +0400

    Updated example

--- VKTestApplication/src/main/java/com/vk/vktestapp/ApiCallActivity.java
+++ VKTestApplication/src/main/java/com/vk/vktestapp/ApiCallActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.vk.sdk.VKSdk;
 import com.vk.sdk.VKUIHelper;
 import com.vk.sdk.api.VKError;
 import com.vk.sdk.api.VKRequest;
@@ -78,71 +80,93 @@ public class ApiCallActivity extends ActionBarActivity {
             myRequest.setRequestListener(mRequestListener);
         }
     }
+	protected void setResponseText(String text) {
+		PlaceholderFragment fragment = getFragment();
+		if (fragment != null && fragment.textView != null) {
+			fragment.textView.setText(text);
+		}
+	}
 
 
+	VKRequestListener mRequestListener = new VKRequestListener()
+	{
+		@Override
+		public void onComplete(VKResponse response)
+		{
+			setResponseText(response.json.toString());
+		}
 
-    VKRequestListener mRequestListener = new VKRequestListener() {
-        @Override
-        public void onComplete(VKResponse response) {
-            getFragment().textView.setText(response.json.toString());
-        }
+		@Override
+		public void onError(VKError error)
+		{
+			setResponseText(error.toString());
+		}
 
-        @Override
-        public void onError(VKError error) {
-            getFragment().textView.setText(error.toString());
-        }
+		@Override
+		public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded,
+		                       long bytesTotal)
+		{
+			// you can show progress of the request if you want
+		}
 
-        @Override
-        public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded,
-        long bytesTotal) {
-            // you can show progress of the request if you want
-        }
+		@Override
+		public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts)
+		{
+			getFragment().textView.append(
+					String.format("Attempt %d/%d failed\n", attemptNumber, totalAttempts));
+		}
+	};
 
-        @Override
-        public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
-            getFragment().textView.append(String.format("Attempt %d/%d failed\n", attemptNumber, totalAttempts));
-        }
-    };
+	@Override
+	protected void onResume()
+	{
+		super.onResume();
+		VKUIHelper.onResume(this);
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        VKUIHelper.onResume(this);
-    }
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		VKUIHelper.onDestroy(this);
+		myRequest.cancel();
+		Log.d(VKSdk.SDK_TAG, "On destroy");
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        VKUIHelper.onDestroy(this);
-    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
+	}
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        VKUIHelper.onActivityResult(this, requestCode, resultCode, data);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		int id = item.getItemId();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+		if (id == android.R.id.home)
+		{
+			finish();
+			return true;
+		}
 
-        if (id == android.R.id.home) {
-            finish();
-            return true;
-        }
+		return super.onOptionsItemSelected(item);
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment
+	{
+		public TextView textView;
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        public TextView textView;
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            View v = inflater.inflate(R.layout.fragment_api_call, container, false);
-            textView = (TextView)v.findViewById(R.id.response);
-            return v;
-        }
-    }
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		                         Bundle savedInstanceState)
+		{
+			View v = inflater.inflate(R.layout.fragment_api_call, container, false);
+			textView = (TextView) v.findViewById(R.id.response);
+			return v;
+		}
+	}
 }
