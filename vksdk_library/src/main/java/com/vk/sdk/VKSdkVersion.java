5acf4399c1093bc77356ad558e603176e0fa5440
Roman Truba <dreddkr@gmail.com>
2014-11-26 18:21:53 +0300

    Updated version 1.3.4

--- vksdk_library/src/main/java/com/vk/sdk/VKSdkVersion.java
+++ vksdk_library/src/main/java/com/vk/sdk/VKSdkVersion.java
@@ -25,6 +25,6 @@ package com.vk.sdk;
  * Contains SDK versions constants
  */
 public class VKSdkVersion {
-    public static final String SDK_VERSION = "1.3.2";
+    public static final String SDK_VERSION = "1.3.4";
     public static final String API_VERSION = "5.21";
 }
